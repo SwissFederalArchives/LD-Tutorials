3f55921ebf883ffdf16845552cfea2bcb78c085b
Benedikt Hitz-Gamper <bhitz@gmx.net>
2024-06-12 10:44:41 +0200

    use service for fedlex

--- content/ext/sparql.py
+++ content/ext/sparql.py
@@ -9,13 +9,19 @@ async def query(query_string, store = "L", set_na = False):
     
     # three Swiss triplestores
     if store == "F":
-        address = 'https://fedlex.data.admin.ch/sparqlendpoint'
+        # as there is a CORS problem at the moment with FEDLEX endpoint, the LINDAS endpoint with a SERVICE clause is used 
+        # address = 'https://fedlex.data.admin.ch/sparqlendpoint'
+        address = 'https://ld.admin.ch/query'
     elif store == "G":
         address = 'https://geo.ld.admin.ch/query'
     elif store == "L":
         address = 'https://ld.admin.ch/query'
     else:
         address = store
+
+    # build the query
+    if store == "F":
+        query_string = insert_service_tail(insert_service_head(query_string))
     
     # try the Post request with help of JS fetch
     # the creation of the request header is a little bit complicated because it needs to be a 
@@ -56,3 +62,42 @@ async def query(query_string, store = "L", set_na = False):
 def display_result(df):
     df = HTML(df.to_html(render_links=True, escape=False))
     display(df)
+
+# helper functions to insert SERVICE clause
+def insert_service_head(original_string):
+    
+    # Find the index of the first occurrence of '{'
+    index = original_string.find('{')
+    
+    # Check if '{' is found
+    if index == -1:
+        # If '{' is not found, return the original string unchanged
+        return original_string
+    
+    # Split the string into two parts
+    before_bracket = original_string[:index + 1]
+    after_bracket = original_string[index + 1:]
+    
+    # Concatenate the parts with the substring inserted in between
+    result_string = before_bracket + 'SERVICE <https://fedlex.data.admin.ch/sparqlendpoint> {' + after_bracket
+    
+    return result_string
+
+def insert_service_tail(original_string):
+    
+    # Find the index of the last occurrence of '}'
+    index = original_string.rfind('}')
+    
+    # Check if '}' is found
+    if index == -1:
+        # If '}' is not found, return the original string unchanged
+        return original_string
+    
+    # Split the string into two parts
+    before_bracket = original_string[:index + 1]
+    after_bracket = original_string[index + 1:]
+    
+    # Concatenate the parts with the additional '}' inserted in between
+    result_string = before_bracket + '}' + after_bracket
+    
+    return result_string
